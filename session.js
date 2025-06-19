508e3f19e513fd54fe89bf504b1cdf40efc5cbb0
Yasiya5 <Sudarshanayasidu764@gmail.com>
2025-06-19 19:50:32 +0530

    Update session.js

--- session.js
+++ session.js
@@ -47,7 +47,7 @@ module.exports = {
   BOT_NUMBER:
     process.env.BOT_NUMBER || "94723628072",
   PASSWORD: 
-    process.env.PASSWORD || "1234",
+    process.env.PASSWORD || "123",
   SUDO: process.env.SUDO
     ? process.env.SUDO.split(",")
     : ["94717775628", "94758775628"],
